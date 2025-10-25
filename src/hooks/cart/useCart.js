f12ecff417924631f6678df6e2d91e71931968ae
mahmudul <mahmudul35-218@diu.edu.bd>
2025-10-25 15:59:08 +0600

    use cart

--- src/hooks/cart/useCart.js
+++
@@ -1,8 +0,0 @@
-import { CartContext } from "@/context/CartContext";
-import { useContext } from "react";
-
-export const useCart = () => {
-  const context = useContext(CartContext);
-  if (!context) throw new Error("useCart must be inside CartProvider");
-  return context;
-};
