beac3e0a701982ad8a0c0c6d4031a83069de2262
mahmudul <mahmudul35-218@diu.edu.bd>
2025-11-10 16:34:28 +0600

    filter api

--- src/lib/api/order/order.js
+++
@@ -1,8 +0,0 @@
-import { privateRequest } from "@/lib/axios";
-
-export const order = async(params={})=>{
-    return await privateRequest.get(`/user/order`,{params});
-}
-export const singleOrder = async(id)=>{
-    return await privateRequest.get(`/user/order/${id}` );
-}
